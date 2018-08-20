a89353fecf8fa09d2bae823217120771e03f8afa
LEEJAEJIN <jinlee1206@naver.com>
2018-08-20 18:48:18 +0900

    Initial Commit

---
+++ DetectObject/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  DetectObject
+//
+//  Created by Euijae Hong on 2018. 8. 20..
+//  Copyright © 2018년 JAEJIN. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
