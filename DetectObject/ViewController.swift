533434c3680dec723d9ffed07302ff3b3efadbc2
LEEJAEJIN <jinlee1206@naver.com>
2018-08-21 15:07:25 +0900

    DetectObject Sample

--- DetectObject/ViewController.swift
+++ DetectObject/ViewController.swift
@@ -1,25 +1,100 @@
 //
 //  ViewController.swift
-//  DetectObject
+//  CameraObjectDetection
 //
-//  Created by Euijae Hong on 2018. 8. 20..
+//  Created by Euijae Hong on 2018. 8. 10..
 //  Copyright © 2018년 JAEJIN. All rights reserved.
 //
 
 import UIKit
+import AVKit
+import Vision
+
 
 class ViewController: UIViewController {
-
+    
+    var shaperLayer = CAShapeLayer()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        setupCamera()
+        view.layer.addSublayer(self.shaperLayer)
+        
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+}
+
+extension ViewController {
+    
+    // Setup Camera
+    private func setupCamera() {
+        
+        let captureSesstion = AVCaptureSession()
+        captureSesstion.sessionPreset = .photo
+        
+        guard let captureDevice = AVCaptureDevice.default(for: .video) else { return }
+        guard let input = try? AVCaptureDeviceInput(device: captureDevice) else { return }
+        
+        captureSesstion.addInput(input)
+        captureSesstion.startRunning()
+        
+        let previewLayer = AVCaptureVideoPreviewLayer(session: captureSesstion)
+        previewLayer.videoGravity = AVLayerVideoGravity.resizeAspectFill
+        view.layer.addSublayer(previewLayer)
+        previewLayer.frame = view.frame
+        
+        print("previewLayer :",previewLayer.frame)
+        
+        let dataOutput = AVCaptureVideoDataOutput()
+        dataOutput.setSampleBufferDelegate(self, queue: DispatchQueue(label: "videoQueue"))
+        captureSesstion.addOutput(dataOutput)
+        
     }
+    
+}
 
 
+//MARK:- AVCaptureVideoDataOutputSampleBufferDelegate
+extension ViewController : AVCaptureVideoDataOutputSampleBufferDelegate {
+    
+    func captureOutput(_ output: AVCaptureOutput, didOutput sampleBuffer: CMSampleBuffer, from connection: AVCaptureConnection) {
+        
+        guard let pixelBuffer : CVPixelBuffer = CMSampleBufferGetImageBuffer(sampleBuffer) else { return }
+        
+        let request = VNDetectRectanglesRequest {[weak self] (req, error) in
+            
+            if let err = error { print(err.localizedDescription) }
+            guard let results = req.results as? [VNRectangleObservation] else { return }
+            guard let firstObservation = results.first else { return }
+            
+            
+            DispatchQueue.main.async {
+                
+                guard let width = self?.view.frame.width else { return }
+                guard let height = self?.view.frame.height else { return }
+                
+                let rectangle = UIBezierPath()
+                
+                rectangle.move(to: CGPoint(x: width * firstObservation.topLeft.x, y: height * firstObservation.topLeft.y))
+                rectangle.addLine(to: CGPoint(x: width * firstObservation.bottomLeft.x, y: height * firstObservation.bottomLeft.y))
+                rectangle.addLine(to: CGPoint(x: width * firstObservation.bottomRight.x, y: height * firstObservation.bottomRight.y))
+                rectangle.addLine(to: CGPoint(x: width * firstObservation.topRight.x, y: height * firstObservation.topRight.y))
+                
+                rectangle.close()
+                
+                self?.shaperLayer.path = rectangle.cgPath
+                self?.shaperLayer.fillColor = UIColor.clear.cgColor
+                self?.shaperLayer.strokeColor = UIColor.blue.cgColor
+                
+            }
+            
+        }
+        
+            try? VNImageRequestHandler(cvPixelBuffer: pixelBuffer, options: [:]).perform([request])
+    }
+    
+
+    
 }
 
